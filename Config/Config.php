b8732b9a68966e7fc8c2cc49b08958c574deead9
ArmandoCS23 CastelanSanchez <1722110105@utectulancingo.edu.mx>
2024-06-20 20:38:46 +0000

    Actualizacion

---
+++ Config/Config.php
@@ -0,0 +1,9 @@
+<?php
+const base_url = "http://localhost/Estadia/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "estadia";
+const charset = "charset=utf8";
+
+?>  
